e4c7a195e3a98123b2258dbe40c6c8f0c7d95f24
Amy Gurski <amygurski@gmail.com>
2020-12-13 00:03:30 -0500

    Solved day 11 part 1

--- 2020/eleven/solution.rb
+++ 2020/eleven/solution.rb
@@ -1,18 +1,45 @@
-seats = File.readlines('test_input.txt', chomp: true).map(&:chars)
+# frozen_string_literal: true
 
-DIRECTIONS = [[-1,-1], [-1,0], [-1, 1], [0,1], [1,1], [1,0], [1,-1],[0,-1]]
+seats = File.readlines('input.txt', chomp: true).map(&:chars)
 
-def get_surroundings(seats, x, y)
+DIRECTIONS = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]].freeze
+
+def get_surroundings(seats, x_index, y_index)
   surroundings = []
-  count = 0
   DIRECTIONS.each do |direction|
-    x_pos = x + direction[0]
-    y_pos = y + direction[1] 
+    x_pos = x_index + direction[0]
+    y_pos = y_index + direction[1]
     if y_pos >= 0 && y_pos < seats.count
-      surroundings << seats[y_pos][x_pos] if (x_pos >= 0 && x_pos < seats[y_pos].count)
+      surroundings << seats[y_pos][x_pos] if x_pos >= 0 && x_pos < seats[y_pos].count
     end
   end
   surroundings
 end
 
-puts "#{get_surroundings(seats, 1, 0)}"
+def get_seat_map(seats)
+  new_seat_map = Array.new(seats.count) { Array.new(seats[0].count) }
+  need_to_repeat = false
+  seats.each_with_index do |row_array, y_index|
+    row_array.each_with_index do |seat, x_index|
+      surroundings = get_surroundings(seats, x_index, y_index)
+      new_seat_map[y_index][x_index] = get_seat_status(seat, surroundings)
+      need_to_repeat = true if new_seat_map[y_index][x_index] != seats[y_index][x_index]
+    end
+  end
+  [need_to_repeat, new_seat_map]
+end
+
+def get_seat_status(seat, surroundings)
+  if seat == 'L' && !surroundings.include?('#')
+    '#'
+  elsif seat == '#' && surroundings.count('#') >= 4
+    'L'
+  else
+    seat
+  end
+end
+
+need_to_repeat = true
+need_to_repeat, seats = get_seat_map(seats) while need_to_repeat
+
+puts "There are #{seats.join(',').count('#')} occupied seats."
