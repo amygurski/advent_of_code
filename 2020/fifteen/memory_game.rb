cfc72046fc2e17eea901aa0997a6d76d4f10ddab
Amy Gurski <amygurski@gmail.com>
2021-01-15 15:20:52 -0500

    refactors day 15

--- 2020/fifteen/memory_game.rb
+++ 2020/fifteen/memory_game.rb
@@ -2,9 +2,10 @@
 
 # Memory game
 class MemoryGame
-  attr_reader :spoken
+  attr_accessor :input
 
-  def initialize
+  def initialize(input)
+    @starting_numbers = input
     @spoken = {}
   end
 
@@ -15,5 +16,23 @@ class MemoryGame
   def add_spoken(number, position)
     @spoken[number] ||= []
     @spoken[number] << position
+  end
+
+  def get_number_spoken(target_number)
+    @starting_numbers.each_with_index { |n, i| add_spoken(n, i + 1) }
+    last_spoken = @starting_numbers.last
+    counter = @starting_numbers.length + 1
+    while counter <= target_number
+      last_spoken = get_last_spoken(last_spoken) if spoken?(last_spoken)
+      add_spoken(last_spoken, counter)
+      counter += 1
+    end
+    @spoken.detect { |_x, y| y.include?(target_number) }.first
+  end
+
+  def get_last_spoken(last_spoken)
+    return 0 if @spoken[last_spoken].length == 1
+
+    @spoken[last_spoken][-1] - @spoken[last_spoken][-2]
   end
 end
