0f071fdb9133f24baa853dfe5952c53cd1be81c0
Amy Gurski <amygurski@gmail.com>
2021-12-04 23:45:22 -0500

    day 4 solution (a mess)

---
+++ 2021/four/part_2.py
@@ -0,0 +1,56 @@
+def get_bingo_values():
+    # Read in bingo values as list of integers
+    with open("input.txt") as f:
+        input_list = [c for c in f.readline().strip().split(',')]
+
+    # return bingo values split into groups of 5
+    return [input_list[x:x+5] for x in range(0, len(input_list), 5)]
+
+def get_boards():
+    with open("input.txt","r") as f:
+        rows = [line.strip().split() for line in f.readlines()[1:] if line.split()]
+    return [rows[x:x+5] for x in range(0, len(rows), 5)]
+
+def calculate_sum(board):
+    nums = [int(item) for sublist in board for item in sublist if item != 'x']
+    return sum(nums)
+
+bingo_values = get_bingo_values()
+boards = get_boards()
+players = [1 for x in range (0,len(boards))] # 1 if still playing, 0 if out
+
+def column_count(board):
+    transposed_board = [[row[i] for row in board] for i in range(len(board[0]))]
+    for row in transposed_board:
+        if row.count('x') == 5:
+            return True
+    return False
+
+found_last = False
+for i in range(len(bingo_values)):
+    # seriously need to break the following mess out into functions
+    for value in bingo_values[i]:
+        for index,board in enumerate(boards):
+            for row in board:
+                for i, item in enumerate(row):
+                    if item == value:
+                        row[i] = 'x'    
+                    if row.count('x') == 5 or column_count(board):
+                        players[index] = 0
+                        if sum(players) == 0:
+                            sum_unmarked_nums = calculate_sum(board)
+                            score = sum_unmarked_nums * int(value)
+                            print(board)
+                            print(f"The last winner has a score of {value} * {sum_unmarked_nums} = {score}")
+                            found_last = True
+                            break
+                # LOLOL what a mess
+                if found_last:
+                    break
+            if found_last:
+                break
+        if found_last:
+            break
+    if found_last:
+        break
+
